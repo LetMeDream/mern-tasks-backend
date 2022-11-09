2eb75077e5a2c4799b0d3bdf0a8d2b61da553850
Let <raulalfonzo66@gmail.com>
2022-11-09 19:41:11 -0400

    Add Github Pages URL to the origin array in our CORS middleware

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors({
-    origin: ['http://127.0.0.1:5173']
+    origin: ['http://127.0.0.1:5173','https://letmedream.github.io/mern-tasks-frontend/']
 }))
 /* Built-in express.json middleware */
 app.use(express.json());
